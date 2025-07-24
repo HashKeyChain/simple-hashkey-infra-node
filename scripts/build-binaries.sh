8b04905e35ba5c0ab285b84f05cf12a45b1ea6c0
maskpp <maskpp266@gmail.com>
2025-07-24 18:02:25 +0800

    update scripts

--- scripts/build-binaries.sh
+++ scripts/build-binaries.sh
@@ -17,6 +17,16 @@ cd $BASE_PATH/optimism/op-proposer && git checkout v1.13.2
 just op-proposer
 cp $BASE_PATH/optimism/op-proposer/bin/op-proposer $BASE_PATH/bin/op-proposer
 
+# build op-batcher
+cd $BASE_PATH/optimism/op-batcher && git checkout v1.13.2
+just op-batcher
+cp $BASE_PATH/optimism/op-batcher/bin/op-batcher $BASE_PATH/bin/op-batcher
+
+# build op-challenger
+cd $BASE_PATH/optimism/op-challenger && git checkout v1.13.2
+just op-challenger
+cp $BASE_PATH/optimism/op-challenger/bin/op-challenger $BASE_PATH/bin/op-challenger
+
 cd $BASE_PATH/op-geth && git checkout optimism
 cd $BASE_PATH/optimism && git checkout develop
 
