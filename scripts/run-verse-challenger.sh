bd042d2cc1e0d7ab74cc2001138473ac3ee98525
maskpp <maskpp266@gmail.com>
2025-08-18 11:36:46 +0800

    update scripts

--- scripts/run-verse-challenger.sh
+++ scripts/run-verse-challenger.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-echo "Starting verse-challenger ..."
-cannon_flags="--cannon-rollup-config=config/$DEPLOYMENT_CONTEXT/rollup.json --cannon-l2-genesis=config/$DEPLOYMENT_CONTEXT/genesis.json --cannon-bin=bin/cannon --cannon-server=bin/op-program --cannon-prestate=bin/prestate-mt64.bin.gz"
+source .envrc
 
-echo "verse-challenger --datadir=data/verse-challenger --l1-eth-rpc=$L1_RPC_URL --l1-beacon=$L1_RPC_URL --l2-eth-rpc=$L2_RPC_URL --rollup-rpc=$OP_NODE_RPC_URL --private-key=data/verse-geth/keyfile.json --trace-type=permissioned --game-factory-address=$(cat $DEPLOYMENT_OUTFILE | jq -r .DisputeGameFactory) --num-confirmations=1 $cannon_flags"
+echo "Starting verse-challenger ..."
+cannon_flags="--cannon-rollup-config=$DEPLOYMENT_CONFIG_PATH/rollup.json --cannon-l2-genesis=$DEPLOYMENT_CONFIG_PATH/genesis.json --cannon-bin=$BASE_PATH/bin/cannon --cannon-server=$BASE_PATH/bin/op-program --cannon-prestate=$BASE_PATH/bin/prestate-mt64.bin.gz"
+
+
+# private-key is the second test account from anvil, the address is 0x70997970C51812dc3A010C7d01b50e0d17dc79C8.
+private_key=0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d
+
+echo "verse-challenger --log.level=debug --datadir=$BASE_PATH/data/verse-challenger --l1-eth-rpc=$L1_RPC_URL --l1-beacon=$L1_RPC_URL --l2-eth-rpc=$L2_RPC_URL --rollup-rpc=$OP_NODE_RPC_URL --private-key=$private_key --trace-type=permissioned --game-factory-address=$(cat $DEPLOYMENT_OUTFILE | jq -r .DisputeGameFactoryProxy) --num-confirmations=1 $cannon_flags"
 
 op-challenger \
-  --datadir=data/verse-challenger \
+  --log.level=debug \
+  --datadir=$BASE_PATH/data/verse-challenger \
   --l1-eth-rpc=$L1_RPC_URL \
   --l1-beacon=$L1_RPC_URL \
   --l2-eth-rpc=$L2_RPC_URL \
   --rollup-rpc=$OP_NODE_RPC_URL \
-  --private-key=data/verse-geth/keyfile.json \
+  --private-key=$private_key \
   --trace-type=permissioned \
-  --game-factory-address=$(cat $DEPLOYMENT_OUTFILE | jq -r .DisputeGameFactory) \
+  --game-factory-address=$(cat $DEPLOYMENT_OUTFILE | jq -r .DisputeGameFactoryProxy) \
   --num-confirmations=1 \
   $cannon_flags
--- scripts/run-verse-proposer.sh
+++ scripts/run-verse-proposer.sh
@@ -4,7 +4,7 @@ source .envrc
 
 echo "Starting op-proposer ..."
 
-echo "op-proposer --log.level=debug --poll-interval=12s --rpc.port=8560 --rollup-rpc=$OP_NODE_RPC_URL --l2oo-address=$(cat $DEPLOYMENT_OUTFILE | jq -r .L2OutputOracleProxy) --private-key=$GS_PROPOSER_PRIVATE_KEY --l1-eth-rpc=$L1_RPC_URL"
+echo "op-proposer --log.level=debug --poll-interval=12s --rpc.port=8560 --rollup-rpc=$OP_NODE_RPC_URL --l2oo-address=$(cat $DEPLOYMENT_OUTFILE | jq -r .L2OutputOracleProxy) --private-key=$GS_PROPOSER_PRIVATE_KEY --l1-eth-rpc=$L1_RPC_URL --num-confirmations=3"
 
 op-proposer \
   --log.level=debug \
@@ -13,4 +13,5 @@ op-proposer \
   --rollup-rpc=$OP_NODE_RPC_URL \
   --l2oo-address=$(cat $DEPLOYMENT_OUTFILE | jq -r .L2OutputOracleProxy) \
   --private-key=$GS_PROPOSER_PRIVATE_KEY \
-  --l1-eth-rpc=$L1_RPC_URL
+  --l1-eth-rpc=$L1_RPC_URL \
+  --num-confirmations=3
