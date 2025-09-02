04e796dbaa290274aef603ac43f23f6a48d399af
maskpp <maskpp266@gmail.com>
2025-09-02 10:35:11 +0800

    update scripts

--- scripts/run-verse-proposer.sh
+++ scripts/run-verse-proposer.sh
@@ -2,13 +2,13 @@
 
 source .envrc
 
-base_flags="--log.level=debug --rpc.port=8560 --rollup-rpc=$OP_NODE_RPC_URL --private-key=$GS_PROPOSER_PRIVATE_KEY --l1-eth-rpc=$L1_RPC_URL"
+base_flags="--log.level=debug --rpc.port=8560 --private-key=$GS_PROPOSER_PRIVATE_KEY --l1-eth-rpc=$L1_RPC_URL"
 
 export proposer_flags=""
 if [ "$USE_FAULT_PROOFS" = "true" ]; then
-  proposer_flags="--game-factory-address=$(cat $DEPLOYMENT_OUTFILE | jq -r .DisputeGameFactoryProxy) --proposal-interval=${PROPOSAL_INTERVAL:-6s}"
+  proposer_flags="--game-factory-address=$(cat $DEPLOYMENT_OUTFILE | jq -r .DisputeGameFactoryProxy) --proposal-interval=${PROPOSAL_INTERVAL:-6s} --game-type=${GAME_TYPE:-0}"
 else
-  proposer_flags="--l2oo-address=$(cat $DEPLOYMENT_OUTFILE | jq -r .L2OutputOracleProxy) --game-type=${GAME_TYPE:-0}"
+  proposer_flags="--l2oo-address=$(cat $DEPLOYMENT_OUTFILE | jq -r .L2OutputOracleProxy) --rollup-rpc=$OP_NODE_RPC_URL"
 fi
 
 misc_flags="--poll-interval=12s --network-timeout=600s --num-confirmations=1 --wait-node-sync=${WAIT_NODE_SYNC:-true}"
