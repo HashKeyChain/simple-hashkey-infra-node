dd304a3ddd6248b9992b477cad0a82f6694afc5d
maskpp <maskpp266@gmail.com>
2025-09-02 10:08:03 +0800

    update scripts

--- scripts/run-verse-node.sh
+++ scripts/run-verse-node.sh
@@ -4,7 +4,7 @@ source .envrc
 
 
 base_flags="--log.level=debug --rpc.addr=0.0.0.0 --l1=${L1_RPC_URL} --l1.rpckind=${L1_RPC_KIND} --l2=http://localhost:8651 --l2.jwt-secret=${BASE_PATH}/data/verse-geth/jwt.txt"
-misc_flags="--sequencer.enabled --l1.epoch-poll-interval=6s --p2p.disable --rpc.enable-admin --p2p.sequencer.key=${GS_SEQUENCER_PRIVATE_KEY} --sequencer.l1-confs=5 --verifier.l1-confs=4"
+misc_flags="--sequencer.enabled --l1.epoch-poll-interval=${L1_BLOCK_TIME}s --p2p.disable --rpc.enable-admin --p2p.sequencer.key=${GS_SEQUENCER_PRIVATE_KEY} --sequencer.l1-confs=5 --verifier.l1-confs=4"
 flags="$base_flags $misc_flags --rollup.config=${OP_NODE_ROLLUP_FILE} --l1.beacon.ignore"
 
 echo "Starting verse-node with rollup config: $OP_NODE_ROLLUP_FILE"
